2f9ff12f68466a3c13e95129587604a4ecef5ee1
iteerP <preetigobindpuri02@gmail.com>
2025-10-18 21:13:58 -0700

    sign up login redirect change

--- src/app/components/login-form.tsx
+++ src/app/components/login-form.tsx
@@ -56,7 +56,7 @@ export function LoginForm({
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`,
         },
       });
       if (error) throw error;
--- src/app/components/sign-up-form.tsx
+++ src/app/components/sign-up-form.tsx
@@ -33,7 +33,7 @@ export function SignUpForm({
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`,
         },
       });
       if (error) throw error;
